bf3bc136b7afa3a2d523c1f0bedc8638cd4d0d0e
Muhammad Bin Sohail <templergreek@gmail.com>
2024-08-12 17:24:50 +0500

    Initial Commit

---
+++ TheWeather/TheWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheWeatherApp.swift
+//  TheWeather
+//
+//  Created by Muhammad Bin Sohail on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TheWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
